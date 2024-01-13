2097fac17794c43128908a4242a67304c0b8c781
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-13 20:57:22 +0100

    automaticallty scale INTENSITY

--- src/bit_tangler.c
+++ src/bit_tangler.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+
+double scaleIntensity(double n) {
+
+
+    double scale = 65.0;
+
+    if (n > 90.0) {scale = 15.0;}
+    else if (n > 70.0) {scale = 25.0;}
+    else if (n > 50.0) {scale = 45.0;}    
+    else if (n > 20.0) {scale = 55.0;}    
+    
+    return n + n * (n / scale);
+}   
 
 size_t getFileSize(const char *filename) {
     FILE *file = fopen(filename, "r");
@@ -36,12 +50,12 @@ void corruptFile(const char *filename, size_t sequences, size_t maxSeqLen) {
     for (size_t i = 0; i < sequences; i++)
     {
         size_t position = rand() % fileSize;
-        size_t seqlength = rand() % maxSeqLen + 1;
+        size_t seqLength = rand() % maxSeqLen + 1;
 
 
         fseek(file, position, SEEK_SET);
 
-        for (size_t i = 0; i < seqlength; i++) {
+        for (size_t i = 0; i < seqLength; i++) {
             //char randomByte = rand() % CHAR_MAX;
             char randomByte = 0x00;
             fwrite(&randomByte, sizeof(char), 1, file);
@@ -56,16 +70,25 @@ int main() {
 
     size_t fileSize = getFileSize(filename);
 
-    size_t const INTENSITY = 50; /* 0 - 100 */
+    size_t INTENSITY = 0; /* 0 - 100 */
+
+
+    INTENSITY = (size_t)scaleNumber((double)INTENSITY);
+    printf("INTENSITY=%lld\n", INTENSITY);
+    //exit(0);
+
+
+    //INTENSITY = (size_t) ((double)INTENSITY * (double)INTENSITY/20);
+
 
     double bytesToCorrupt = (double)fileSize * ((double)INTENSITY/100);
-    printf("%f\n", bytesToCorrupt);
+    printf("bytesToCorrupt=%f\n", bytesToCorrupt);
 
-    size_t maxSeqLen = (size_t) (bytesToCorrupt * 0.025);
-    printf("%lld\n", maxSeqLen);
+    size_t maxSeqLen = (size_t) ceil((bytesToCorrupt * 0.003));
+    printf("maxSeqLen=%lld\n", maxSeqLen);
 
     size_t sequences = (size_t) (bytesToCorrupt / (double)maxSeqLen);
-    printf("%lld\n", sequences);
+    printf("sequences=%lld\n", sequences);
 
 
     corruptFile(filename, sequences, maxSeqLen);
