f80593f6631f169448af22623ba9af5441f76064
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-13 14:49:03 +0100

    use correct filename and write only 0x00 for testing

--- src/bit_tangler.c
+++ src/bit_tangler.c
@@ -10,24 +10,20 @@ void corruptFile(const char *filename, size_t numBytesToCorrupt) {
         exit(EXIT_FAILURE);
     }
 
-    // Get the file size
+    // get file size
     fseek(file, 0, SEEK_END);
     long fileSize = ftell(file);
-    rewind(file);
+    fseek(file, 0, SEEK_END);
 
-    // Seed for random number generation
     srand((unsigned int)time(NULL));
 
-    // Overwrite random parts of the file
     for (size_t i = 0; i < numBytesToCorrupt; ++i) {
-        // Generate a random position within the file
         long position = rand() % fileSize;
 
-        // Move the file pointer to the random position
         fseek(file, position, SEEK_SET);
 
-        // Overwrite one byte with a random value
-        char randomByte = rand() % 256;
+        //char randomByte = rand() % 256;
+        char randomByte = 0x00;
         fwrite(&randomByte, sizeof(char), 1, file);
     }
 
@@ -35,11 +31,8 @@ void corruptFile(const char *filename, size_t numBytesToCorrupt) {
 }
 
 int main() {
-    const char *filename = "example.txt";
+    const char *filename = "test.txt";
     size_t numBytesToCorrupt = 10;
-
-    // Create a backup of the original file before corrupting it
-    system("cp example.txt example_backup.txt");
 
     corruptFile(filename, numBytesToCorrupt);
 
