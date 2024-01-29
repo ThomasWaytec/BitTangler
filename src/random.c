0bff232e9c78bd5e3ffbe5195b44fb3d382ea301
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-29 22:44:00 +0100

    change randSizeT to largeRand

--- src/random.c
+++ src/random.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 
-size_t randSizeT(size_t lower, size_t upper) {
 
-    size_t range = upper + 1 - lower;
+
+long long int largeRand(long long int lower, long long int upper) {
+
+    long long int range = upper + 1 - lower;
     
-    size_t prelimitedRandNum = rand() ^ (rand() << 15) ^ ((size_t) rand() << 30); /* to return larger numbers than RAND_MAX */
-    size_t randNum = prelimitedRandNum % range + lower;
+    long long int prelimitedRandNum = rand() ^ (rand() << 15) ^ ((long long int) rand() << 30); /* to return larger numbers than RAND_MAX */
+    long long int randNum = prelimitedRandNum % range + lower;
 
     return randNum;
 }
