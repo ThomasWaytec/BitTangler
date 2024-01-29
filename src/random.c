b6cb895799dcec463c4399302b22247effb2c14d
ThomasWaytec <thomas.waytec@gmail.com>
2024-01-29 22:32:07 +0100

    return size_t instead of int; make upper limit inclusive

--- src/random.c
+++ src/random.c
@@ -2,13 +2,10 @@
 
 size_t randSizeT(size_t lower, size_t upper) {
 
-    /* seed the RNG */
-    srand((unsigned int)time(NULL));
-
-    size_t range = upper - lower + 1;
+    size_t range = upper + 1 - lower;
     
     size_t prelimitedRandNum = rand() ^ (rand() << 15) ^ ((size_t) rand() << 30); /* to return larger numbers than RAND_MAX */
-    int randNum = prelimitedRandNum % range + lower;
+    size_t randNum = prelimitedRandNum % range + lower;
 
     return randNum;
 }
